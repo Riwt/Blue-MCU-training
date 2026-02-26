9a91d6f84c9e0f47edaf426bd2dff9f36772058e
Zhang <260419306+Riwt@users.noreply.github.com>
2026-02-26 16:47:22 +0800

    PWM_control_led_complete

--- PWM_control_led/main.c
+++ PWM_control_led/main.c
@@ -1,16 +1,18 @@
 #include <STC15F2K60S2.H>
+#define N 3
 sbit s4=P3^3;
 sbit s5=P3^2;
 sbit s6=P3^1;
 sbit s7=P3^0;
-volatile unsigned char count=50;
+volatile unsigned char count=0;
 volatile unsigned char tick_100us=0;
 volatile unsigned char tick_1ms=0;
 volatile bit flag_10ms=0;
-unsigned char pwm_duty=0;
+bit is_led_run=0;
+unsigned char pwm_duty=50;
 unsigned char btn_what=0;
 unsigned char smg_code[11]={0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0x80,0x90,0xFF};
-unsigned char show_buf[2]={10,10};
+unsigned char show_buf[N]={10,10,10};
 void System_Init(void)
 {
 	P2=0xA0; P0=0x00; P2=0x00;
@@ -28,7 +30,7 @@ void Nixie_scan(void){
 	P0=smg_code[show_buf[pos]];
 	P2=0xE0; P2=0x00;
 	pos++;
-	if(pos>1) pos=0;
+	if(pos>2) pos=0;
 }
 unsigned char Btn_Read(void){
 	P44=0;
@@ -61,28 +63,42 @@ void Btn_loop(void){
 		break;
 }
 }
+void Update_show(void){
+	unsigned char i=0;
+	unsigned char s=pwm_duty;
+	for(i=0;i<N;i++){
+		show_buf[N-i-1]=s%10;
+		s/=10;}
+}
 void Control_pwm(void){
-	
+	if(is_led_run==1&&btn_what!=7){
+		btn_what=0; return ;}
 	switch (btn_what)
 {
-	case 4:
+	case 4://全灭
+	Set_led(0xFF); pwm_duty=0;
 		break;
-	case 5:
+	case 5://  暗
+		if(pwm_duty>=10) pwm_duty-=10;
+		else pwm_duty=0;
 		break;
-	case 6:
+	case 6://  亮
+		if(pwm_duty<=90) pwm_duty+=10;
+		else pwm_duty=100;
 		break;
-	case 7:
+	case 7://  自动
+		is_led_run=!is_led_run;
 		break;
+}	btn_what=0;
+	Update_show();
 }
+void Pwm_count(void){
+    count = (count + 1) % 100;
+    Set_led((count < pwm_duty) ? 0xFE : 0xFF);  // 低电平点亮
 }
 void Timer0_Isr(void) interrupt 1
 {
-	count++;
-	if(count==pwm_duty){
-		Set_led(0xFE);}
-	if(count>=100){
-		Set_led(0xFF);
-		count=0;}
+	Pwm_count();
 	tick_100us++;
 	if(tick_100us>=10){
 		tick_1ms++; tick_100us=0;
@@ -92,7 +108,6 @@ void Timer0_Isr(void) interrupt 1
 		flag_10ms=1;
 	}}
 }
-
 void Timer0_Init(void)		//100微秒@11.0592MHz
 {
 	AUXR |= 0x80;			//定时器时钟1T模式
@@ -102,11 +117,31 @@ void Timer0_Init(void)		//100微秒@11.0592MHz
 	TF0 = 0;				//清除TF0标志
 	TR0 = 1;				//定时器0开始计时
 	ET0 = 1;				//使能定时器0中断
-	EA=0;
+	EA=1;
+}
+void Breath_led(void){
+	static unsigned char flag_20ms=0;
+	static unsigned char pwm_d=0;
+	static bit is_right=1;
+	flag_20ms++;
+	if(flag_20ms>2){
+		flag_20ms=0; 
+		pwm_d++;
+		if(pwm_d>100){
+			is_right=!is_right; pwm_d=0;}
+	if(is_right) pwm_duty=pwm_d;
+		else pwm_duty=100-pwm_d;
+	Update_show();}
 }
 void main(){
-	
+	System_Init();
+	Timer0_Init();
 	while(1){
-	
+	if(flag_10ms){
+		flag_10ms=0;
+		Btn_loop();
+		Control_pwm();
+	if(is_led_run){
+		Breath_led();}}
 }
 }
